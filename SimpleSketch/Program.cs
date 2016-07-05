be39f0ec7dfd8ecd74fda74e0e397589f8a4df21
russell <russell>
2016-07-05 15:01:37 -0400

    Purposely removing error to test the teamcity api build check

--- SimpleSketch/Program.cs
+++ SimpleSketch/Program.cs
@@ -17,6 +17,6 @@ namespace SimpleSketch
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        
+        }
     }
 }
